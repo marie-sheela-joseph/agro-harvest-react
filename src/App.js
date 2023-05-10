e23608d21b728664f028402ac7e9f4d86048ca3c
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-05-10 02:23:11 +0200

    Core section completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import BestHarvesting from "./components/BestHarvesting";
 import './App.css'
 import Choose from "./components/Choose";
+import Core from "./components/Core";
 function App() {
   return (
     <>
       <BestHarvesting />
       <Choose />
+      <Core />
     </>
   )
 }
